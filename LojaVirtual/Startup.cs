db3a8f892092607cb23598981523d144fec3f836
Glauber Marques <75267767+gvfmarques@users.noreply.github.com>
2021-05-31 22:05:35 -0500

    Feature: Creating views, email connections and validation

--- LojaVirtual/Startup.cs
+++ LojaVirtual/Startup.cs
@@ -51,7 +51,7 @@ namespace LojaVirtual
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Produto}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
